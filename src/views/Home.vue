53924054a4a98d1a0dc575066905e62587d7a8fa
Benjamin Werner <bewr@pm.me>
2023-12-20 15:35:40 +0100

    add uppercase switch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
-  <v-container>
+  <v-container class="mt-16 pt-16">
     <v-row justify="center">
       <v-col cols="12" sm="8" md="6" class="d-flex flex-column justify-center">
         <v-text-field label="Enter Text" v-model="text" outlined></v-text-field>
+        <v-switch
+          color="primary"
+          v-model="uppercase"
+          label="Convert to Uppercase"
+        ></v-switch>
         <v-btn color="primary" @click="generateHash">Generate MD5 Hash</v-btn>
         <h1 v-if="hash" class="mt-8 text-center" style="word-wrap: break-word">
           <span class="highlight">{{ hash.substring(0, 12) }}</span
@@ -15,10 +20,17 @@
 
 <script lang="ts" setup>
 import md5 from "md5";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const text = ref("");
 const hash = ref("");
+const uppercase = ref(true);
+
+watch(text, (newVal) => {
+  if (uppercase.value) {
+    text.value = newVal.toUpperCase();
+  }
+});
 
 function generateHash() {
   hash.value = md5(text.value);
